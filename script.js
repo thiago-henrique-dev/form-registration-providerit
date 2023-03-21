6f33d2135be183eab54d9f0b87310aa3ad59ff08
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-03-21 19:45:30 -0300

    formated

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ function clearData() {
     document.getElementById("estado-civil").value = "solteiro";
   }
 
-  function formatCPF(cpfInput) {
-let cpf = cpfInput.value.replace(/\D/g, ''); // remove tudo que não for nmro do input
+function formatCPF(cpfInput) {
+    let cpf = cpfInput.value.replace(/\D/g, ''); 
 
-cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); // coloca um ponto após os primeiros 3 dígitos
-cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); // coloca um ponto após os próximos 3 dígitos
-cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // coloca um traço após os últimos 2 dígitos
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); 
 
-cpfInput.value = cpf; // atualiza o valor do input com o CPF formatado
+    cpfInput.value = cpf; 
 }
 
 function formatRG(rgInput) {
-let rg = rgInput.value.replace(/\D/g, ''); // remove tudo que não for numero do input
+    let rg = rgInput.value.replace(/\D/g, ''); 
 
-rg = rg.replace(/(\d{3})(\d)/, '$1.$2'); // coloca um ponto após os primeiros 2 dígitos
-rg = rg.replace(/(\d{3})(\d)/, '$1.$2'); // coloca um ponto após os proxs 3 dígitos
-rg = rg.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // coloca um traco após os ults 2 dígitos
+    rg = rg.replace(/(\d{3})(\d)/, '$1.$2'); 
+    rg = rg.replace(/(\d{3})(\d)/, '$1.$2'); 
+    rg = rg.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); 
 
-rgInput.value = rg; // atualizar o valor do input com o RG formatado
+    rgInput.value = rg;
 }
