d6488fd1c3dad7d327a1cc39527b9e9f051ed5d0
thiagohenriquedev <thiagohenriquedev@gmail.com>
2023-03-22 21:50:26 -0300

    add database sqlite e create function add

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
+const db = openDatabase('provider', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+async function createTable() {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS pessoas (nome TEXT, rg TEXT, cpf TEXT, endereco TEXT, numero INTEGER, bairro TEXT, cidade TEXT, estado TEXT, complemento TEXT, sexo TEXT, datanascimento DATE, estadocivil TEXT)',
+        [],
+        resolve,
+        (_, error) => reject(error)
+      );
+    });
+  });
+}
+
+async function saveDate() {
+  const nome = document.getElementById('nome').value;
+  const rg = document.getElementById('rg').value;
+  const cpf = document.getElementById('cpf').value;
+  const endereco = document.getElementById('endereco').value;
+  const numero = document.getElementById('numero').value;
+  const bairro = document.getElementById('bairro').value;
+  const cidade = document.getElementById('cidade').value;
+  const estado = document.getElementById('estado').value;
+  const complemento = document.getElementById('complemento').value;
+  const sexo = document.getElementById('sexo').value;
+  const datanascimento = document.getElementById('data-nascimento').value;
+  const estadocivil = document.getElementById('estado-civil').value;
+
+  await createTable();
+
+  return new Promise((resolve, reject) => {
+    db.transaction(
+      tx => {
+        tx.executeSql(
+          'INSERT INTO pessoas (nome, rg, cpf, endereco, numero, bairro, cidade, estado, complemento, sexo, datanascimento, estadocivil)',
+          [nome, rg, cpf, endereco, numero, bairro, cidade, estado, complemento, sexo, datanascimento, estadocivil],
+          resolve,
+          (_, error) => reject(error)
+        );
+      },
+      (_, error) => reject(error)
+    );
+  });
+}
+
 function clearData() {
-    const elements = ["nome", "rg", "cpf", "endereco", "data-nascimento", "endereco", "bairro", "compl",
+    const elements = ["nome", "rg", "cpf", "endereco", "data-nascimento", "endereco", "bairro", "complemento",
      "estado", "residencia", "cidade"];
     
     elements.forEach((element) => {
